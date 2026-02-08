2576c1fbfd490f49f5375b4b6cf12ac49f0f8b4f
Gustavo Maiolli <gusmaiolli758@gmail.com>
2026-02-08 14:48:57 -0300

    Adição de feedbacks cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Products from './components/Products/Products';
 import About from './components/About/About';
+import Feedback from './components/Feedback/Feedback';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero></Hero>
         <Products></Products>
         <About></About>
+        <Feedback></Feedback>
       </header>
     </div>
   );
---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,72 @@
+import './Feedback.css'
+
+function Feedback(){
+    return(
+        <div className="Feedback">
+            <div className="Feedback__container">
+                    <div className="Feedback__card">
+                        <div className="Feedback__user">
+                            <div className="Feedback__user__icon">
+                                <img src="imgs/berit/berit-logo.png"></img>
+                            </div>
+                            <div className="Feedback__user__name">
+                                <p>Bruno</p>
+                            </div>
+                        </div>
+                    <div className="Feedback__stars">
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                    </div>
+                    <div className="Feedback__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mattis gravida diam in bibendum.</p>
+                    </div>
+                </div>
+                <div className="Feedback__card">
+                    <div className="Feedback__user">
+                        <div className="Feedback__user__icon">
+                            <img src="assets/user-solid-full.svg"></img>
+                        </div>
+                        <div className="Feedback__user__name">
+                            <p>Lucas</p>
+                        </div>
+                    </div>
+                    <div className="Feedback__stars">
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-regular-full.svg"></img>
+                        <img src="assets/star-regular-full.svg"></img>
+                    </div>
+                    <div className="Feedback__text">
+                        <p>Nullam elit leo, convallis eget tempus sed, tristique eu justo. Pellentesque mattis gravida diam in bibendum.</p>
+                    </div>
+                </div>
+                <div className="Feedback__card">
+                    <div className="Feedback__user">
+                        <div className="Feedback__user__icon">
+                            <img src="assets/user-solid-full.svg"></img>
+                        </div>
+                        <div className="Feedback__user__name">
+                            <p>Maiolli</p>
+                        </div>
+                    </div>
+                    <div className="Feedback__stars">
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-solid-full.svg"></img>
+                        <img src="assets/star-regular-full.svg"></img>
+                    </div>
+                    <div className="Feedback__text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor lacus sed nisl viverra finibus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Feedback;
